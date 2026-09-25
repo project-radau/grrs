23f637a99f8ba0e587da73a37409714bc95957a5
radau <project.radau@gmail.com>
2026-09-25 21:24:07 +0200

    with bufreader

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+// https://rust-cli.github.io/book/tutorial/impl-draft.html
+
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use clap::Parser;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    path: std::path::PathBuf,
 }
+
+
+fn main() -> std::io::Result<()> {
+    let args = Cli::parse();
+
+    let f = File::open(&args.path)?;
+    let reader = BufReader::new(f);
+
+    // Gehe jede Zeile der Datei einzeln durch
+    for line in reader.lines() {
+        let line = line?; // Liest die Zeile ein und fängt eventuelle I/O-Fehler ab
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+
+    Ok(())
+}
+
+
